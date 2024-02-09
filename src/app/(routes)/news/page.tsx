2012bcd66e5393c4df772b83dc082c9676022745
binithekid <bini.tek1@gmail.com>
2024-02-09 12:48:22 +0000

    update

--- src/app/(routes)/news/page.tsx
+++ src/app/(routes)/news/page.tsx
@@ -1,32 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import styles from "../../Styles/modules/News/post.module.scss";
 import PostCard from "../../Components/News/PostCard";
 import { client } from "../../utils/client";
 
-export default function News() {
-  async function getPosts() {
-    const response = await client.getEntries({ content_type: "post" });
-    const posts = await response.items;
+async function getPosts() {
+  const response = await client.getEntries({ content_type: "post" });
+  const posts = await response.items;
 
-    return posts;
-  }
+  return posts;
+}
 
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchedPosts = await getPosts();
-        setPosts(fetchedPosts);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+export default async function News() {
+  const posts = await getPosts();
 
   return (
     <main className={styles.news_page}>
@@ -34,7 +20,7 @@ export default function News() {
         <h1>News</h1>
 
         <div className={styles.news_wrapper}>
-          {posts?.map((post: any, index: number) => (
+          {posts.map((post: any, index: number) => (
             <PostCard post={post} key={index} />
           ))}
         </div>
